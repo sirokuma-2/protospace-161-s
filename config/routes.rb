aca5852e5e7afa20bb682d1ead4465c53608a457
d-miyabi <msgh3110+works@gmail.com>
2024-01-21 19:00:29 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   get 'prototypes/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "prototypes#index"
 end
