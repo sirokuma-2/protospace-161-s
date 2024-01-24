092d14c49a9ff8546cef66a1eca33191f106a825
d-miyabi <msgh3110+works@gmail.com>
2024-01-24 21:08:12 +0900

    newとcreateのコントローラーの設定

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,10 +1,20 @@
 class PrototypesController < ApplicationController
   def index
   end
-end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    Prototype.create(prototype_params)
+    redirect_to
+  end
 
   private
 
   def prototype_params
     params.require(:prototype).permit(:content, :image).merge(user_id: current_user.id)
   end
+
+end
