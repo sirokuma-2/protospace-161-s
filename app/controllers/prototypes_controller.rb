a245b4d62e03d04080b4f8b2ac8a1576222ff7d5
d-miyabi <msgh3110+works@gmail.com>
2024-01-24 20:52:56 +0900

    画像の保存を許可するストロングパラメーターの編集

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -2,3 +2,9 @@ class PrototypesController < ApplicationController
   def index
   end
 end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:content, :image).merge(user_id: current_user.id)
+  end
