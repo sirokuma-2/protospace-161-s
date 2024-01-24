44e8470c162e95e7acb7b7fb20cff42ed606fa47
d-miyabi <msgh3110+works@gmail.com>
2024-01-24 19:29:14 +0900

    マイグレーションファイルに外部キーの記述

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_22_183519) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_24_092917) do
+  create_table "prototypes", charset: "utf8", force: :cascade do |t|
+    t.string "title"
+    t.text "catch_copy"
+    t.text "concept"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
