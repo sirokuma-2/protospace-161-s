a8334bdc84cac3fe9ea1e4070eb4e540da9fb3c6
d-miyabi <msgh3110+works@gmail.com>
2024-01-24 18:48:11 +0900

    マイグレーションファイルの編集

--- db/migrate/20240124092917_create_prototypes.rb
+++ db/migrate/20240124092917_create_prototypes.rb
@@ -1,7 +1,9 @@
 class CreatePrototypes < ActiveRecord::Migration[7.0]
   def change
     create_table :prototypes do |t|
-
+      t.string :title
+      t.text :catch_copy
+      t.text :concept
       t.timestamps
     end
   end
