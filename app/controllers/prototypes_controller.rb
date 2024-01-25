5af8148ad2cf3dc411364cd78510d61d1af2ae73
d-miyabi <msgh3110+works@gmail.com>
2024-01-25 21:51:01 +0900

    投稿機能の実装（画像以外）

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,6 @@
 class PrototypesController < ApplicationController
   def index
+    @prototype = Prototype.new
   end
 
   def new
@@ -8,13 +9,24 @@ class PrototypesController < ApplicationController
 
   def create
     Prototype.create(prototype_params)
-    redirect_to
+    redirect_to root_path
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
 
   def prototype_params
-    params.require(:prototype).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:prototype).permit(:title, :catch_copy, :concept )
   end
 
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,7 +1,7 @@
 class Prototype < ApplicationRecord
-  belongs_to :user
+  # belongs_to :user
   has_one_attached :image
-  validates :string, presence: true
-  validates :text, presence: true
-  validates :image, presence: true
+  # validates :string, presence: true
+  # validates :text, presence: true
+  # validates :image, presence: true
 end
