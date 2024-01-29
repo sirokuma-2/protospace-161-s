0ce5fb3afb92e49759d1610915170cb0565784ab
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-01-30 05:10:09 +0900

    rubocopの実施

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,7 @@ class CommentsController < ApplicationController
   end
 
   private
+
   def comment_params
     params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
   end
